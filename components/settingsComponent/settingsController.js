28c527d9153aaea2bdbc46a007ca5b08879ca256
Ben <ben.medeiros@planttherapy.com>
2023-08-07 20:39:53 -0600

    use media aspect ratio to handle formatting for phones; simplified the settings Element

--- components/settingsComponent/settingsController.js
+++ components/settingsComponent/settingsController.js
@@ -1,21 +1,11 @@
 'use strict';
 
-//adds a temp css transition class and removes it when complete;
-//needed so that user resize:both isn't isn't animated
-function addTempTransitionClass(el, cssClassName) {
-    el.classList.add(cssClassName);
-    Promise.allSettled(el.getAnimations().map(ani => ani.finished)).then(() => {
-        el.classList.remove(cssClassName);
-    })
-}
-
 function collapseSettingElement(event) {
     if (settingsExpanded) {
         const gameConfigEl = document.querySelector('.collapsible');
         settingsExpanded = false;
         if (!gameConfigEl.classList.contains('collapsed')) {
             gameConfigEl.classList.add('collapsed');
-            addTempTransitionClass(gameConfigEl, 'collapsing');
         }
     }
 }
@@ -27,7 +17,6 @@ function expandSettingElement(event) {
         settingsExpanded = true;
         if (gameConfigEl.classList.contains('collapsed')) {
             gameConfigEl.classList.remove('collapsed');
-            addTempTransitionClass(gameConfigEl, 'expanding');
         }
     }
 }
